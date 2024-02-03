1c93d3557bda4a65f2cf08dec66ab8c5c9a2925d
deniska69 <gostev.dy@yandex.ru>
2024-02-04 03:17:08 +0700

    rf constants 🔤

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,12 @@
-import { CANVAS_SIZE, GRID } from "./Config";
+const enum GRID {
+  countRows = 15,
+  countColumns = 9,
+  cellWidthAndHeigth = 30,
+}
+
+const enum CANVAS_SIZE {
+  width = 800,
+  height = 600,
+}
 
 export { CANVAS_SIZE, GRID };
