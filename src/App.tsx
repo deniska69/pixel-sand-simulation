c9608798ec701c902421ef436ec109d4d6dfc270
deniska69 <gostev.dy@yandex.ru>
2024-02-04 03:42:39 +0700

    add draw grid logic to canvas 🥱

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,43 @@
+import { ChangeEvent, useState } from "react";
 import "./App.css";
 import Canvas from "components/Canvas";
+import Controls from "components/Controls";
+import { HStack } from "ui";
+import { CANVAS_SIZE, GRID } from "constants";
+import { isNumber } from "helpers";
 
-function App() {
-  return <Canvas />;
-}
+const MAX_ROWS = Math.floor(CANVAS_SIZE.height / GRID.cellWidthAndHeigth);
+const MAX_COLUMNS = Math.floor(CANVAS_SIZE.height / GRID.cellWidthAndHeigth);
+
+const App = () => {
+  const [countRows, setCountRows] = useState<number>(GRID.countRows);
+  const [countColumns, setCountColumns] = useState<number>(GRID.countColumns);
+
+  const onChangeCountRows = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    if (!isNumber(value)) return;
+    setCountRows(Number(value));
+  };
+
+  const onChangeCountColumns = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    if (!isNumber(value)) return;
+    setCountColumns(Number(value));
+  };
+
+  return (
+    <HStack>
+      <Canvas />
+      <Controls
+        {...{
+          countRows,
+          countColumns,
+          onChangeCountRows,
+          onChangeCountColumns,
+        }}
+      />
+    </HStack>
+  );
+};
 
 export default App;
--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,9 +1,13 @@
-import { useEffect, useRef } from "react";
-import { CANVAS_SIZE } from "constants/Size";
+import { useEffect, useRef, memo } from "react";
 import "./Canvas.css";
+import { CANVAS_SIZE, GRID } from "constants";
 
 const Canvas = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const width = CANVAS_SIZE.width;
+  const height = CANVAS_SIZE.height;
+  const centerHeight = height / 2;
+  const centerWidth = width / 2;
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -19,16 +23,52 @@ const Canvas = () => {
     }
 
     context.fillStyle = "white";
-    context.fillRect(0, 0, 100, 100);
-  }, []);
 
-  return (
-    <canvas
-      ref={canvasRef}
-      width={CANVAS_SIZE.width}
-      height={CANVAS_SIZE.height}
-    />
-  );
+    drawCenterLines(context);
+    drawGrid(context, 15, 9);
+  });
+
+  function drawCenterLines(ctx: CanvasRenderingContext2D) {
+    ctx.setLineDash([5, 3]);
+    ctx.lineWidth = 0.4;
+
+    ctx.beginPath();
+    ctx.moveTo(centerWidth, 0);
+    ctx.lineTo(centerWidth, height);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(0, centerHeight);
+    ctx.lineTo(width, centerHeight);
+    ctx.stroke();
+  }
+
+  function drawGrid(
+    ctx: CanvasRenderingContext2D,
+    countRows: number,
+    countColumns: number
+  ) {
+    ctx.setLineDash([]);
+    ctx.lineWidth = 2;
+
+    const startPointX =
+      centerWidth - Math.round((countColumns * GRID.cellWidthAndHeigth) / 2);
+    const startPointY =
+      centerHeight - Math.round((countRows * GRID.cellWidthAndHeigth) / 2);
+
+    for (let x = 0; x < countColumns; x++) {
+      for (let y = 0; y < countRows; y++) {
+        ctx.strokeRect(
+          startPointX + GRID.cellWidthAndHeigth * x,
+          startPointY + GRID.cellWidthAndHeigth * y,
+          GRID.cellWidthAndHeigth,
+          GRID.cellWidthAndHeigth
+        );
+      }
+    }
+  }
+
+  return <canvas ref={canvasRef} width={width} height={height} />;
 };
 
-export default Canvas;
+export default memo(Canvas);
